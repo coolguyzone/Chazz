2ee930c4955599ce5533c7b2ee61f0a4993f6da8
Alex Krawiec <alexanderkrawiec@gmail.com>
2017-04-07 08:59:21 -0700

    got cheese image rendering correctly and added some css to make page fit better on laptops and center cheeses

--- src/components/cheese.js
+++ src/components/cheese.js
@@ -1,7 +1,10 @@
 import React from 'react';
 
 const Cheese = () => (
-  <div className="col-md-6 cheese">Cheeese</div>
+  <div className="col-md-6 cheese">
+    <div>Cheese Name</div>
+    <img src={require('../img/cheese-icon.png')} alt="cheese"/>
+  </div>
 )
 
 export default Cheese;
