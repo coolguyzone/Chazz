678fe6324729e0eeb945cc4cdbc41250aad11541
Alex Krawiec <alexanderkrawiec@gmail.com>
2017-04-07 17:03:10 -0700

    changes

---
+++ src/components/CheeseButton.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Cheese from './cheese';
+
+const CheeseButton = () => (
+  <div className="cheese-button">
+  <button className="btn btn-default">I've Tried It!</button>
+</div>
+);
+
+export default CheeseButton;
--- src/components/cheese.js
+++ src/components/cheese.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import CheeseButton from './CheeseButton';
 
 const Cheese = () => (
   <div className="col-md-6 cheese">
     <div>Cheese Name</div>
     <img src={require('../img/cheese-icon.png')} alt="cheese"/>
+    <CheeseButton />
   </div>
 )
 
