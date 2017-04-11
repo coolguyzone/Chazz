d1d993350a03feb539b24c2d24fc3c7d1bda4873
Alex Krawiec <alexanderkrawiec@gmail.com>
2017-04-11 13:12:40 -0700

    martys player bug fix

--- src/components/player.js
+++ src/components/player.js
@@ -4,37 +4,28 @@ import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import arrayShuffler from '../helpers/array-shuffler.js';
 import axios from 'axios';
-
-
 const mapStateToProps = (state, ownProps) => {
   return {
     uriArray: state.playlist.uriArray,
     next: state.playlist.next
   };
 };
-
 const mapDispatchToProps = (dispatch) => {
   return bindActionCreators ({ initialPlaylist, nextPlaylist }, dispatch);
 };
-
-
 class Player extends Component {
-
-  componentWillMount() {
-    this.props = {
-      ...this.props,
-      uriArray: ["spotify:user:gita1503:playlist:2qkXmULVYKfaQVLYQiuMJA"],
-      next: 0
-    };
-  }
-
   render() {
-
+    let uri = '';
+    if (this.props.uriArray.length === 0) {
+      uri = "spotify:user:gita1503:playlist:2qkXmULVYKfaQVLYQiuMJA";
+    }
+    else{
+      uri = this.props.uriArray[this.props.next];
+    }
     return (
-
       <div className="col-md-4" id="player">
         <iframe src={
-          `https://embed.spotify.com/?uri=${this.props.uriArray[this.props.next]}`
+          `https://embed.spotify.com/?uri=${uri}`
         } width="300" height="350" frameBorder="0" allowTransparency="true"></iframe>
         <button onClick={
           (event) => {
@@ -46,11 +37,7 @@ class Player extends Component {
             }
           }} className="btn btn-default" >New Jazz Playlist</button>
       </div>
-
     )
   }
 }
-
-
-
 export default connect (mapStateToProps, mapDispatchToProps)(Player);
